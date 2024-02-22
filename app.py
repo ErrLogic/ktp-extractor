5e666b6121597667dd4749cbf4fd4fcde22396d3
ErrLogic <tuyul.elektronik@gmail.com>
2024-02-23 01:02:57 +0700

    add new requirement, re-work flow

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from distutils.log import debug
 from flask import Flask
 from core.router import Router
-from flask_cors import CORS, cross_origin
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
