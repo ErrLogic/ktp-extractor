ac1bd4e3dbede8af183f94cdaaea8c31e770df4f
ErrLogic <tuyul.elektronik@gmail.com>
2024-02-23 02:48:37 +0700

    logging

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import logging
 from flask import Flask
 from core.router import Router
 from flask_cors import CORS
+
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
