fe14dba2fb75899185b2960f3854c30f08219a63
ErrLogic <tuyul.elektronik@gmail.com>
2024-02-23 03:19:27 +0700

    regex dash

--- ktpocr/extractor.py
+++ ktpocr/extractor.py
@@ -51,23 +51,24 @@ class KTPOCR(object):
         return res
     
     def extract(self, extracted_result):
-        replace_table = str.maketrans({'—': '', '“': '', '.': '', ':': '', '|': '', '-': ''})
+        replace_table = str.maketrans({'—': '', '“': '', '.': '', ':': '', '|': '', '/': ''})
+        remove_dash = str.maketrans({'-': ' '})
         
         for word in extracted_result.split("\n"):
             if "NIK" in word:
                 word = word.split(':')
-                self.result.nik = self.nik_extract(word[-1].replace(" ", "")).replace("NIK", "").translate(replace_table)
+                self.result.nik = self.nik_extract(word[-1].replace(" ", "")).replace("NIK", "").translate(replace_table).translate(remove_dash)
                 continue
 
             if "Nama" in word:
                 word = word.split(':')
-                self.result.nama = word[-1].replace('Nama ','').translate(replace_table)
+                self.result.nama = word[-1].replace('Nama ','').translate(replace_table).translate(remove_dash)
                 continue
 
             if "Tempat" in word:
                 word = word.split(':')
-                self.result.tanggal_lahir = re.search("([0-9]{2}\-[0-9]{2}\-[0-9]{4})", word[-1])[0]
-                self.result.tempat_lahir = word[-1].replace(". "+self.result.tanggal_lahir, '')
+                self.result.tanggal_lahir = re.search("([0-9]{2}\-[0-9]{2}\-[0-9]{4})", word[-1])[0].translate(replace_table)
+                self.result.tempat_lahir = word[-1].replace(". "+self.result.tanggal_lahir, '').translate(replace_table)
                 continue
 
             if 'Darah' in word:
@@ -79,19 +80,19 @@ class KTPOCR(object):
                 word = word.split(':')
 
                 try:
-                    self.result.golongan_darah = re.search("(O|A|B|AB)", word[-1])[0].translate(replace_table)
+                    self.result.golongan_darah = re.search("(O|A|B|AB)", word[-1])[0].translate(replace_table).translate(remove_dash)
                 except:
                     self.result.golongan_darah = '-'
 
             if 'Alamat' in word:
-                self.result.alamat = self.word_to_number_converter(word).replace("Alamat ","").translate(replace_table)
+                self.result.alamat = self.word_to_number_converter(word).replace("Alamat ","").translate(replace_table).translate(remove_dash)
 
             if 'NO.' in word:
-                self.result.alamat = self.result.alamat + ' '+word.translate(replace_table)
+                self.result.alamat = self.result.alamat + ' '+word.translate(replace_table).translate(remove_dash)
 
             if "Kecamatan" in word:
                 if len(word.split(':')) > 1:
-                    self.result.kecamatan = word.split(':')[1].strip().translate(replace_table)
+                    self.result.kecamatan = word.split(':')[1].strip().translate(replace_table).translate(remove_dash)
 
             if "Desa" in word:
                 wrd = word.split()
@@ -99,11 +100,11 @@ class KTPOCR(object):
                 for wr in wrd:
                     if not 'desa' in wr.lower():
                         desa.append(wr)
-                self.result.kelurahan_atau_desa = ''.join(wr)
+                self.result.kelurahan_atau_desa = ''.join(wr).translate(replace_table).translate(remove_dash)
 
             if 'Kewarganegaraan' in word:
                 if len(word.split(':')) > 1:
-                    self.result.kewarganegaraan = word.split(':')[1].strip().translate(replace_table)
+                    self.result.kewarganegaraan = word.split(':')[1].strip().translate(replace_table).translate(remove_dash)
 
             if 'Pekerjaan' in word:
                 wrod = word.split()
@@ -114,16 +115,16 @@ class KTPOCR(object):
                 self.result.pekerjaan = ' '.join(pekerjaan).replace('Pekerjaan', '').strip().translate(replace_table)
 
             if 'Agama' in word:
-                self.result.agama = word.replace('Agama',"").strip().translate(replace_table)
+                self.result.agama = word.replace('Agama',"").strip().translate(replace_table).translate(remove_dash)
 
             if 'Perkawinan' in word:
-                self.result.status_perkawinan = word.split(':')[1].translate(replace_table)
+                self.result.status_perkawinan = word.split(':')[1].translate(replace_table).translate(remove_dash)
 
             if "RTRW" in word:
                 word = word.replace("RTRW",'')
                 if len(word.split('/')) > 1:
-                    self.result.rt = word.split('/')[0].strip().translate(replace_table)
-                    self.result.rw = word.split('/')[1].strip().translate(replace_table)
+                    self.result.rt = word.split('/')[0].strip().translate(replace_table).translate(remove_dash)
+                    self.result.rw = word.split('/')[1].strip().translate(replace_table).translate(remove_dash)
 
     def master_process(self):
         raw_text = self.process(self.image)
